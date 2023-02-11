597d1128e576acd3a9366cde0c8b3a17a734408e
Benjamin Okwuwolu <k.webzzy@gmail.com>
2023-02-11 01:20:13 -0600

    fixing linting tmp issues

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,5 +2,6 @@
 calculator function
 """
 
+
 def add(x,y):
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,9 @@
-import unittest
+# import unittest
 from django.test import SimpleTestCase
 from app import calc
+
 
 class MyTestCase(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)  # add assertion here
-
-
